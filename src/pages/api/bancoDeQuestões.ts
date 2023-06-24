bfc276b8b3c4aa0158bffc06dbb1a564d6bbea87
Lucio Noleto <luciolnoleto@gmail.com>
2023-06-24 01:45:23 -0300

    correção no banco de questoes

--- src/pages/api/bancoDeQuestões.ts
+++ src/pages/api/bancoDeQuestões.ts
@@ -2,7 +2,7 @@ import QuestaoModel from "../../../model/questao";
 import RespostaModel from "../../../model/resposta";
 
 const questoes: QuestaoModel[] = [
-    new QuestaoModel(202, 'Qual bicho transmite a Doença de Chagas?', [
+    new QuestaoModel(201, 'Qual bicho transmite a Doença de Chagas?', [
         RespostaModel.errada('Abelha'),
         RespostaModel.errada('Barata'),
         RespostaModel.errada('Pulga'),
